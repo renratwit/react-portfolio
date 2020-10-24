74aba36eb34c0c26d2fbe0b882c6c8f4c301894b
renratwit <raymond.ren9@gmail.com>
2020-10-24 18:07:23 -0400

    try build #2

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Typical from 'react-typical';
-import react_icon from '../images/react_icon.jpg'
-import python_logo from '../images/python_logo.png'
+// import react_icon from '../images/react_icon.jpg'
+// import python_logo from '../images/python_logo.png'
 import './icons.css';
 
 
@@ -17,10 +17,10 @@ function About() {
             />
         </div>
 
-        <div className='icons'>
+        {/* <div className='icons'>
             <img className='react_icon icon' src={react_icon} alt='React Icon'></img>
             <img className='python_icon icon' src={python_logo} alt='Python Icon'></img>
-        </div>
+        </div> */}
         
             
         
